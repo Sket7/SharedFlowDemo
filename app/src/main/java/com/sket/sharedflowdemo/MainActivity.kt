4e0bcea8083fe1485b15fbe35dd15c7ef1d79e47
Sket7 <egor1212123@mail.ru>
2026-04-28 07:33:38 +0700

    Колона с вью моделью

--- app/src/main/java/com/sket/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/sket/sharedflowdemo/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sket.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 
@@ -34,7 +43,25 @@ class MainActivity : ComponentActivity() {
 fun StartScreen(
     modifier: Modifier = Modifier, viewModel: DemoViewModel = viewModel()
 ) {
-    Text(
-        text = "Hello !", modifier = modifier
-    )
+    val sharedFlow = viewModel.sharedFlow
+    val messages = remember { mutableStateListOf<Int>() }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(Unit) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect { value ->
+                messages.add(value)
+            }
+        }
+    }
+
+    LazyColumn(modifier = modifier) {
+        items(messages) { value ->
+            Text(
+                text = "Collected Value = $value",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
